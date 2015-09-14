cba79eb125ff087e0a46a594d0ac87346911b76b
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-09-14 13:04:03 -0400

    Add options to `pip_call` builder DSL

--- lib/omnibus/builder.rb
+++ lib/omnibus/builder.rb
@@ -94,12 +94,12 @@ module Omnibus
     #   the pip subcommand to execute
     #
     # @return [void]
-    def pip_call(subcommand)
+    def pip_call(subcommand, options = {})
      pip_path = "\"#{install_dir}/embedded/bin/pip\""
      if Ohai['platform'] == "windows"
        pip_path = "\"#{windows_safe_path(install_dir)}\\embedded\\Scripts\\pip.exe\""
      end
-     command("#{pip_path} #{subcommand}")
+     command("#{pip_path} #{subcommand}", options)
     end
     expose :pip_call
 
